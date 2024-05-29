df8131d0612333d6dd48fed208402ed26f106b36
Allen <Allen.araya.rosales@est.una.ac.cr>
2024-05-29 10:23:35 -0600

    MOdificacion, opciones con menu y pimpieza de pantalla principal

--- OperacionesAritmeticas.cpp
+++ OperacionesAritmeticas.cpp
@@ -1,23 +1,50 @@
 #include <iostream>
-#include <chrono>
-//#include "CreadorOperaciones.cpp"
 #include "Utils.cpp"
+
 using namespace std;
-using namespace std::chrono;
+
+void showMenu() {
+    cout << "Seleccione una opción:" << endl;
+    cout << "1. Calcular el seno de la matriz." << endl;
+    cout << "2. Calcular el coseno de la matriz." << endl;
+    cout << "3. Calcular la potencia de la matriz." << endl;
+    cout << "4. Realizar un trabajo intensivo (sin + cos + potencia)." << endl;
+    cout << "5. Salir del programa." << endl;
+}
 
 int main() {
     Utils utils;
-    double time;
-    utils.timeIntensiveWork(1);
-    utils.calculaSin(1);
-    utils.calculaCos(1);
-    utils.calculaPow(1);
 
-    cout << endl;
-    cout << endl;
-    cout << endl;
-    
-    time =  utils.timeSin(100); // Ejecutar 100 iteraciones de cálculo de seno
+    int option;
+    do {
+        showMenu();
+        cout << "Opción: ";
+        cin >> option;
+
+        switch (option) {
+        case 1:
+            utils.calculaSin(1);
+            break;
+        case 2:
+            utils.calculaCos(1);
+            break;
+        case 3:
+            utils.calculaPow(1);
+            break;
+        case 4:
+            utils.timeIntensiveWork(1);
+            break;
+        case 5:
+            cout << "Saliendo del programa." << endl;
+            break;
+        default:
+            cout << "Opción no válida. Por favor, seleccione una opción válida." << endl;
+            break;
+        }
+    } while (option != 5);
+
+    double time;
+    time = utils.timeSin(100); // Ejecutar 100 iteraciones de cálculo de seno
     std::cout << "Tiempo en paralelo es: SIN( " << time << " milisegundos )" << std::endl;
 
     time = utils.timeCos(100); // Ejecutar 100 iteraciones de cálculo de seno
@@ -27,4 +54,6 @@ int main() {
     std::cout << "Tiempo en paralelo es: POW( " << time << " milisegundos )" << std::endl;
 
     utils.saveMatrix();
+
+    return 0;
 }
